bfb8b98b8bbb818dbdde33f935a8a8080aded6d8
jisrobi <robiulhasanjisan0711@gmail.com>
2024-03-20 23:30:32 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+
+#DAY1
+print ("hello ,world") #" " its for string ;
+print ("hello 'and' ,world") #modify
+print ('hello "said" world')
+print('hello "said" jisan world\n'"Alternatively you can just \"escape\" the quote")#\n for new line
+print('hello' + " " +'jisan')#space really improtant
+#input("what is your name ?")#a prompt for the user;
+#print("hello " + input("what is your name ?"))
+#print("hello " + input("what is your name ?"))
+#num1= int(input())
+#num2= int(input())
+#print(num1*num2)
+#numoflentgh = len(input())#len calculate number of latter in a string
+#print(numoflentgh)
+#project : band name
+print("Wellcome to the band  name generator ")
+name = input("Whats your name ? \n")
+city  = input("which city did you growth up? \n")
+print( "RESULT " + name + " " + city)
+
+
