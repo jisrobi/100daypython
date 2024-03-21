752e74f2e6066e240174f426c9dbf4bef9acb0b3
jisrobi <145006342+jisrobi@users.noreply.github.com>
2024-03-21 22:41:03 +0600

    Update main.py

--- main.py
+++ main.py
@@ -20,3 +20,35 @@ city  = input("which city did you growth up? \n")
 print( "RESULT " + name + " " + city)
 
 
+#Day2
+
+print("hello"[4])#finding index
+print("123"+"345")#""string
+print(342+654)#int
+print(3.1416)#float
+#num_char =len(input("Whats  your  name ?"))
+#print("your name has " + num_new_char +" characters .")
+b=(123)
+print(type(b))
+a=float(123)
+print(type(a))
+c=str(123)
+print(type(c))
+print( 70 + float(100.4))
+print (str(40)+ str(100))
+print(2**3)
+print(6/2)
+# # Taking user input for height and weight
+# height = input("Enter your height in meters: ")
+# weight = input("Enter your weight in kilograms: ")
+#
+# # Converting input strings to appropriate data types
+# height_as_int = int(height)
+# weight_as_float = float(weight)
+#
+# # Calculating BMI
+# bmi = weight_as_float / (height_as_int ** 2)
+#
+# # Printing the BMI value
+# print("Your BMI is:", bmi)
+print(round(3/6,1))
